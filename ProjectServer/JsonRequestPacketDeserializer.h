ebc387a240a2b8b77d5c58036529e8ff5f961902
Gavriel Linoy <kraserultra@gmail.com>
2024-04-03 00:04:19 +0300

    Create LoginRequest and SigunpRequest structs

--- ProjectServer/JsonRequestPacketDeserializer.h
+++ ProjectServer/JsonRequestPacketDeserializer.h
@@ -2,6 +2,19 @@
 #include <string>
 #include <algorithm>
 
+typedef struct LoginRequest
+{
+	std::string username;
+	std::string password;
+} LoginRequest;
+
+typedef struct SignupRequest
+{
+	std::string username;
+	std::string password;
+	std::string email;
+};
+
 class JsonRequestPacketDeserializer
 {
 	public:
