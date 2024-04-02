93c04b549df05e4cf0ffabf33e492d7bdcbd84c8
Gavriel Linoy <kraserultra@gmail.com>
2024-04-02 23:07:20 +0300

    Add Response structs for error, login and signup

--- ProjectServer/JsonResponsePacketSerializer.h
+++ ProjectServer/JsonResponsePacketSerializer.h
@@ -1,6 +1,22 @@
 #pragma once
+#include <string>
 #include <vector>
 #include "json.hpp"
+
+typedef struct ErrorResponse
+{
+	std::string message;
+} ErrorResponse;
+
+typedef struct LoginResponse
+{
+	unsigned int status;
+};
+
+typedef struct SignupResponse
+{
+	unsigned int status;
+} SignupResponse;
 
 class JsonResponsePacketSerializer
 {
