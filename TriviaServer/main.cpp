0278bc2968c04635c31020061c4c8c89ea558560
Gavriel Linoy <kraserultra@gmail.com>
2024-04-11 17:17:15 +0300

    Finish uploading V1.0.1

--- TriviaServer/main.cpp
+++ TriviaServer/main.cpp
@@ -1,25 +1,13 @@
 #pragma comment (lib, "ws2_32.lib")
 #include "MagshMessageServer.h"
 #include "WSAInitializer.h"
-#include "JsonResponsePacketSerializer.h"
-#include "JsonRequestPacketDeserializer.h"
 #include <iostream>
 #include <fstream>
 
 // In a lot of places in the code we pass to function constant reference (const Bla&)
 // to an object and not the object itself, 
 
-void run_server();
-
-void run_json_tests();
-
 int main()
-{
-	// run_server();
-	run_json_tests();
-}
-
-void run_server()
 {
 	// Q: why is this try necessarily ?
 	// A: we want to know what happened that made the server crush
@@ -40,28 +28,4 @@ void run_server()
 	{
 		std::cout << "Unknown exception in main !" << std::endl;
 	}
-}
-
-void run_json_tests()
-{
-	ErrorResponse er = {"ERORR",};
-
-	std::vector<unsigned char> vec = JsonResponsePacketSerializer::serializeResponse(er);
-	
-	// test print
-	for (int i = 0; i < vec.size(); i++)
-	{
-		std::cout << vec[i];
-	}
-	std::cout << std::endl;
-	try
-	{
-		JsonRequestPacketDeserializer::deserializeLoginRequest(vec);
-	}
-	catch (...)
-	{
-		std::cout << "json error..." << std::endl;
-	}
-
-	vec.clear();
 }
