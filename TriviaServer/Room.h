d08ed1bf458c7079494dc6d052788cf42bc6bcdd
Gavriel Linoy <kraserultra@gmail.com>
2024-04-25 21:46:53 +0300

    Add Room class skeleton and RoomData struct

---
+++ TriviaServer/Room.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "includes.hpp"
+
+typedef struct RoomData
+{
+	unsigned int id;
+	std::string name;
+	unsigned int maxPlayers;
+	unsigned int numOfQuestionsInGame;
+	unsigned int timePerQuestion;
+	unsigned int isActive;
+};
+
+class Room
+{
+
+};
+
