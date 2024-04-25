bd1d6cdeb108cb820639835189a7477f45d8d313
Gavriel Linoy <kraserultra@gmail.com>
2024-04-25 12:03:10 +0300

    Resolve last issues

--- TriviaServer/IRequestHandler.h
+++ TriviaServer/IRequestHandler.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <vector>
+#include <ctime>
+
+typedef struct RequestInfo
+{
+	unsigned int id;
+	std::time_t recievalTime;
+	std::vector<unsigned char> buffer;
+} RequestInfo;
+
+typedef struct RequestResult
+{
+	std::vector<unsigned char> buffer;
+	class IReqeustHandler* newHandler;
+} RequestResult;
+
 class IRequestHandler
 {
-
+	public:
+		virtual bool isRequestRelevant(const RequestInfo& requestInfo) = 0;
+		virtual RequestResult handleRequest(const RequestInfo& requestInfo) = 0;
 };
--- TriviaServer/LoginRequestHandler.h
+++ TriviaServer/LoginRequestHandler.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "IRequestHandler.h"
+#include "Responses.hpp"
+#include "JsonResponsePacketSerializer.h"
+#include "JsonRequestPacketDeserializer.h"
+#include "LoginManager.h"
+
 class LoginRequestHandler : virtual public IRequestHandler
 {
-
+	public:
+		bool isRequestRelevant(const RequestInfo& requestInfo) override;
+		RequestResult handleRequest(const RequestInfo& requestInfo) override;
 };
 
