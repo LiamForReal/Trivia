3bc58801a67f5f2798d6574987f6015418d1578b
Gavriel Linoy <kraserultra@gmail.com>
2024-04-26 14:37:44 +0300

    Finish serialization if UML is ok

--- TriviaServer/JsonResponsePacketSerializer.cpp
+++ TriviaServer/JsonResponsePacketSerializer.cpp
@@ -193,6 +193,53 @@ std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const
 	return vec;
 }
 
+std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const CreateRoomResponse& createRoomResponse)
+{
+	std::vector<unsigned char> vec(INIT_VEC_SIZE);
+	// Add Response Code
+	vec[0] = ((unsigned char)(CREATE_ROOM_RC));
 
+	unsigned int len = 0;
 
+	json srJson = {
+		{"status", createRoomResponse.status},
+	};
 
+	std::string srJsonStr = srJson.dump();
+
+	// Insert Message Length Into Vector
+	len = (unsigned int)(srJsonStr.size()); // possible lose of data for 64 bits.
+	std::memcpy(vec.data() + INC, &len, BYTES_TO_COPY);
+
+	// Insert Message Into Vector
+	vec.insert(vec.end(), srJsonStr.begin(), srJsonStr.end());
+
+	return vec;
+}
+
+std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(const GetHighScoreResponse& getHighScoreResponse)
+{
+	std::vector<unsigned char> vec(INIT_VEC_SIZE);
+	// Add Response Code
+	vec[0] = ((unsigned char)(GET_HIGH_SCORE_RC));
+
+	unsigned int len = 0;
+
+	json srJson = {
+		{"players", json(getHighScoreResponse.statistics)},
+		{"status", getHighScoreResponse.status},
+	};
+
+	std::string srJsonStr = srJson.dump();
+
+	std::cout << "[Json Strings and Vecs] DEBUG: " << srJsonStr << std::endl;
+
+	// Insert Message Length Into Vector
+	len = (unsigned int)(srJsonStr.size()); // possible lose of data for 64 bits.
+	std::memcpy(vec.data() + INC, &len, BYTES_TO_COPY);
+
+	// Insert Message Into Vector
+	vec.insert(vec.end(), srJsonStr.begin(), srJsonStr.end());
+
+	return vec;
+}
