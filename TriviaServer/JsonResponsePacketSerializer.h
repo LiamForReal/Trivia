836eda67d9f91ed57633c85af0b0571eebc4d092
Gavriel Linoy <kraserultra@gmail.com>
2024-04-26 12:50:52 +0300

    Add functions skeletons to Serializer

--- TriviaServer/JsonResponsePacketSerializer.h
+++ TriviaServer/JsonResponsePacketSerializer.h
@@ -11,5 +11,13 @@ class JsonResponsePacketSerializer
 		static std::vector<unsigned char> serializeResponse(const ErrorResponse& errorResponse);
 		static std::vector<unsigned char> serializeResponse(const LoginResponse& loginResponse);
 		static std::vector<unsigned char> serializeResponse(const SignupResponse& signupResponse);
+		static std::vector<unsigned char> serializeResponse(const LogoutResponse& logoutResponse);
+		static std::vector<unsigned char> serializeResponse(const GetRoomsResponse& logoutResponse);
+		static std::vector<unsigned char> serializeResponse(const GetPlayersInRoomResponse& logoutResponse);
+		static std::vector<unsigned char> serializeResponse(const JoinRoomResponse& logoutResponse);
+		static std::vector<unsigned char> serializeResponse(const CreateRoomResponse& logoutResponse);
+		static std::vector<unsigned char> serializeResponse(const GetHighScoreResponse& logoutResponse);
+
+
 };
 
