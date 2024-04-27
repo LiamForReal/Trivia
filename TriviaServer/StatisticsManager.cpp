697bb5b5b3566f0d59b93437008a20942fa34a8f
liam meir <liamneir11@gmail.com>
2024-04-27 03:17:25 +0300

    alost finish only remains statistics manager and menuRequestHendler that I dont know witch function to make

--- TriviaServer/StatisticsManager.cpp
+++ TriviaServer/StatisticsManager.cpp
@@ -4,12 +4,11 @@ vector<string> StatisticsManager::getHighScore() const
 {
 	vector<string> fiveBestScores;
 	list<QuestionStatistics> questionsStatistics = _db->getQuestionsStatistics();
+	list<User> users = _db->getUsers();
 	for (auto it = questionsStatistics.begin(); it != questionsStatistics.end(); ++it)
 	{
-		//complit this 
-		if()
+
 	}
-	return s;
 }
 vector<string> StatisticsManager::getUserStatistics(string username) const
 {
