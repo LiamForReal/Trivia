5ec19ab307a03b5a4f2b8972c836416537004e85
Gavriel Linoy <kraserultra@gmail.com>
2024-05-01 18:15:58 +0300

    Add Request structs to Requests.hpp

--- TriviaServer/Requests.hpp
+++ TriviaServer/Requests.hpp
@@ -13,3 +13,21 @@ typedef struct SignupRequest
 	std::string password;
 	std::string email;
 } SignupRequest;
+
+typedef struct GetPlayersInRoomRequest
+{
+	unsigned int roomId;
+} GetPlayersInRoomRequest;
+
+typedef struct JoinRoomRequest
+{
+	unsigned int roomId;
+} JoinRoomRequest;
+
+typedef struct CreateRoomRequest
+{
+	std::string roomName;
+	unsigned int maxUsers;
+	unsigned int questionsCount;
+	unsigned int answerTimeout;
+} CreateRoomRequest;
