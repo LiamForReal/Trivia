54df6d6e69f02a1f166bd78a19f0ae5b7dab2ed7
liam meir <liamneir11@gmail.com>
2024-05-02 01:08:53 +0300

    added questions

--- TriviaServer/StatisticsManager.cpp
+++ TriviaServer/StatisticsManager.cpp
@@ -36,12 +36,12 @@ vector<string> StatisticsManager::getHighScore() const
 }
 string StatisticsManager::getUserStatistics(string username) const
 {
-	std::vector<string> userStatistics; //user statistics: number of games | number of correct answers | number of wrong answers | avrage time for answer
+	string userStatistics; //user statistics: number of games | number of correct answers | number of wrong answers | avrage time for answer
 	double correctAnswers = 0.0;
-	userStatistics.push_back(std::to_string(_db->getNumOfPlayerGames(username)));
+	userStatistics += std::to_string(_db->getNumOfPlayerGames(username)) + "|";
 	correctAnswers = _db->getNumOfCorrectAnswers(username);
-	userStatistics.push_back(std::to_string(correctAnswers));
-	userStatistics.push_back(std::to_string(_db->getNumOfTotalAnswers(username) - correctAnswers));
-	userStatistics.push_back(std::to_string(_db->getPlayerAverageAnswerTime(username)));
-	return userStatistics;
+	userStatistics += std::to_string(correctAnswers) + "|";
+	userStatistics += std::to_string(_db->getNumOfTotalAnswers(username) - correctAnswers) + "|";
+	userStatistics += std::to_string(_db->getPlayerAverageAnswerTime(username));
+	return userStatistics; 
 }
