c727a31a0775858636eb3475d3056e54b712d07d
Gavriel Linoy <kraserultra@gmail.com>
2024-05-10 17:30:03 +0300

    More requests + c'tors

--- TriviaClient/TriviaClient/Requests.cs
+++ TriviaClient/TriviaClient/Requests.cs
@@ -8,11 +8,24 @@ namespace TriviaClient
 {
     internal struct LoginRequest
     {
+        public LoginRequest(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
         public string username;
         public string password;
     }
     internal struct SignupRequest
     {
+        public SignupRequest(string username, string password, string email)
+        {
+            this.username = username;
+            this.password = password;
+            this.email = email;
+        }
+
         public string username;
         public string password;
         public string email;
@@ -20,11 +33,37 @@ namespace TriviaClient
 
     internal struct JoinRoomRequest
     {
+        public JoinRoomRequest(uint roomId)
+        {
+            this.roomId = roomId;
+        }
+
         public uint roomId;
     }
 
     internal struct GetPlayersInRoomRequest
     {
+        public GetPlayersInRoomRequest(uint roomId)
+        {
+            this.roomId = roomId;
+        }
+
         public uint roomId;
+    }
+
+    internal struct CreateRoomRequest
+    {
+        public CreateRoomRequest(string roomName, uint maxUsers, uint questionCount, uint answerTimeout)
+        {
+            this.roomName = roomName;
+            this.maxUsers = maxUsers;
+            this.questionCount = questionCount;
+            this.answerTimeout = answerTimeout;
+        }
+
+        public string roomName;
+        public uint maxUsers;
+        public uint questionCount;
+        public uint answerTimeout;
     }
 }
