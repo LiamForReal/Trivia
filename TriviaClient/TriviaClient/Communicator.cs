5fb51d1178d2eb0ab55bf411f231b62c2d7d19a1
liam meir <liamneir11@gmail.com>
2024-05-10 21:59:50 +0300

    update

--- TriviaClient/TriviaClient/Communicator.cs
+++ TriviaClient/TriviaClient/Communicator.cs
@@ -7,28 +7,12 @@ using System.Threading.Tasks;
 
 namespace TriviaClient
 {
-    internal static class Communicator
+    internal class Communicator
     {
-        public static List<byte> Serialize(string jsonMsg, byte status)
-        {
-        
-            List<byte> list = new List<byte>();
-            list.Add(status);
+        //private loginRequest lr;
 
-            // example:
-            // LoginResponse loginResponse = LoginResponse.Deserialize(list);
+        //public sendLoginRequest (one per class)
 
-
-            jsonMsg = JsonConvert.SerializeObject(jsonMsg, Formatting.Indented);
-            jsonMsg = jsonMsg.Replace("'", "\"");
-            jsonMsg = jsonMsg.Substring(1, jsonMsg.Length - 2);
-
-            UInt32 length = (UInt32)(jsonMsg.Length);
-            list.AddRange(BitConverter.GetBytes(length));
-
-            list.AddRange(Encoding.ASCII.GetBytes(jsonMsg));
-
-            return list;
-        }
+        //public getMsgFromServer (one) 
     }
 }
