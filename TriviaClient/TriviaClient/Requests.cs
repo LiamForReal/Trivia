218cc94805c6c3c170d2d908fb97398d0f12ef8d
Gavriel Linoy <kraserultra@gmail.com>
2024-05-10 17:25:33 +0300

    Adding structs for requests #1

--- TriviaClient/TriviaClient/Requests.cs
+++ TriviaClient/TriviaClient/Requests.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace TriviaClient
 {
-    internal class Requests
+    internal struct LoginRequest
     {
+        public string username;
+        public string password;
+    }
+    internal struct SignupRequest
+    {
+        public string username;
+        public string password;
+        public string email;
+    }
 
+    internal struct JoinRoomRequest
+    {
+        public uint roomId;
+    }
+
+    internal struct GetPlayersInRoomRequest
+    {
+        public uint roomId;
     }
 }
