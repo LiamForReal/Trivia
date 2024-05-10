fe49b9211698df022d66fcaf9775b80d2ac4436d
Gavriel Linoy <kraserultra@gmail.com>
2024-05-10 17:39:10 +0300

    Adding Responses

---
+++ TriviaClient/TriviaClient/Responses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient
+{
+    internal class Responses
+    {
+        internal struct LoginResponse
+        {
+            public LoginResponse(uint status)
+            {
+                this.status = status;
+            }
+
+            public uint status;
+        }
+
+        internal struct SignupResponse
+        {
+            public SignupResponse(uint status)
+            {
+                this.status = status;
+            }
+
+            public uint status;
+        }
+
+
+    }
+}
