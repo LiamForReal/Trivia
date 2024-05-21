5c0ab2a30ba7e44e2ca11ab4eaf2f76a16948339
Gavriel Linoy <kraserultra@gmail.com>
2024-05-21 20:47:44 +0300

    Fix status codes for leave and add Resquests and Responses to client of CreateRoom LeaveRoom StartGame

---
+++ TriviaClient/TriviaClient/StartGameRequest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaClient
+{
+    internal class StartGameRequest
+    {
+        private List<byte> Serialize()
+        {
+
+            List<byte> list = new List<byte>();
+            list.Add((byte)Cods.ResponseCode.START_GAME_RC);
+            return list;
+        }
+
+        public void SendToServer(NetworkStream clientStream)
+        {
+            SocketTools.SendToServer(Serialize(), clientStream);
+        }
+
+        public StartGameResponse GetFromServer(NetworkStream clientStream)
+        {
+            StartGameResponse LogOutResponse = StartGameResponse.Deserialize(SocketTools.GetMsgFromServer(clientStream));
+            return LogOutResponse;
+        }
+
+        internal struct StartGameResponse
+        {
+            public uint status;
+            public StartGameResponse(uint status)
+            {
+                this.status = status;
+            }
+
+            public static StartGameResponse Deserialize(List<byte> list)
+            {
+                StartGameResponse response = new StartGameResponse((uint)list[0]);
+                return response;
+            }
+        }
+    }
+}
