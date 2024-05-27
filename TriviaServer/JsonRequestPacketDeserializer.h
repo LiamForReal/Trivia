9b33c2301c7520697a2ca3c1b8615252743b4b64
Gavriel Linoy <kraserultra@gmail.com>
2024-05-27 21:06:13 +0300

    Implement deserializeSubmitAnswerRequest in server

--- TriviaServer/JsonRequestPacketDeserializer.h
+++ TriviaServer/JsonRequestPacketDeserializer.h
@@ -14,5 +14,6 @@ class JsonRequestPacketDeserializer
 		static GetPlayersInRoomRequest deserializeGetPlayersInRoomRequest(const std::vector<unsigned char>& buffer);
 		static JoinRoomRequest deserializeJoinRoomRequest(const std::vector<unsigned char>& buffer);
 		static CreateRoomRequest deserializeCreateRoomRequest(const std::vector<unsigned char>& buffer);
+		static SubmitAnswerRequest deserializeSubmitAnswerRequest(const std::vector<unsigned char>& buffer);
 };
 
